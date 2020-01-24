fd176e531efb951aa6304750af3e280f35815cbd
Daniel Fong <daniel.fong.mv@gmail.com>
2020-01-24 09:51:55 -0700

    getting ready to retro wireframe

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -14,11 +14,11 @@ const handleAboutMeClick = function() {
             </section>
                 <p class='first-paragraph-aboutMe'>Which retrospectively is why I was drawn to web development;</p>
                 <p>the process of unraveling nebulous visions and ideas and manifesting them into a coherent experience compels me to continue learning on a daily basis.</p>
-                <p>I have often found that learning is enhanced by others, and when given the opportunity I enjoy working (and playing) with those who share my motivation for continuous improvement and learning while bringing a different perspective to the table.</p>
+                <p>I have often found that learning is enhanced by others, and I enjoy working with those who share my motivation for continuous improvement and learning while bringing a different perspective to the table.</p>
                 <p>I am goal and outcome oriented, a diligent worker in any area I apply myself to, and a thoughtful problem solver.</p>
                     <img class='climbing-pic' src='img/free-range.jpg'/>
                 <p>Being based in Denver, CO makes it convenient to put myself to the test mentally and physically. Much of my prior work experience has been in the outdoor industry, and if I’m not honing my skills behind a computer screen or sleeping, there’s a 99% chance I’m out with friends working my way through physical problems rock climbing. </p>
-                <p>Well, either that or snowboarding, playing video games, table tennis, golfing, or enjoying a scrumptious snack. I am equally passionate about the outdoors and web development, and have been excited to witness the rapidly expanding junction between the two.</p>
+                <p>I am equally passionate about the outdoors and web development, and have been excited to witness the rapidly expanding junction between the two.</p>
             <h3>Education</h3>
                 <ul class='education-ul'>
                     <li>Thinkful Engineering Immersion Program (January 2020 - ongoing)</li>
